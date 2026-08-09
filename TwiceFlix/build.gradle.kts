6f3b97fdf370b8a87bb3a93bf3f9ee90d2e81e38
boestar234-debug <boestar234@gmail.com>
2026-08-09 18:56:03 +0800

    Update build.gradle.kts

--- TwiceFlix/build.gradle.kts
+++ TwiceFlix/build.gradle.kts
@@ -1,3 +1,29 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+cloudstream {
+    // The fixed repository setting!
+    setRepo("boestar234-debug", "sakau4", "github")
+}
+
 android {
-    namespace = "com.sakau4.twiceflix"
+    namespace = "com.sakau2.twiceflix"
+    compileSdk = 34
+    
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    compileOnly("com.github.recloudstream:cloudstream:pre-release")
 }
