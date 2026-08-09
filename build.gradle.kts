f6572400d86fc8dba0618d06e3b8fd9ecd95937c
boestar234-debug <boestar234@gmail.com>
2026-08-09 18:56:09 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import com.android.build.gradle.BaseExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-
 buildscript {
     repositories {
         google()
@@ -9,8 +6,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
     }
 }
 
@@ -19,33 +16,5 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
-    }
-}
-
-// This applies the Cloudstream plugin and Android settings to all sub-folders automatically
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-extensions.configure<CloudstreamExtension>("cloudstream") {
-        setRepo("boestar234-debug", "sakau4", "github")
-    }
-
-    extensions.configure<BaseExtension>("android") {
-        compileSdkVersion(34)
-        defaultConfig {
-            minSdk = 21
-            targetSdk = 34
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
-        }
-    }
-
-    dependencies {
-        val implementation by configurations
-        implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
     }
 }
