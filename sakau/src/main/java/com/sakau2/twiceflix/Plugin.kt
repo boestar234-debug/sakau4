3778a05a3e0699489ddee15255a02765d969bb97
boestar234-debug <boestar234@gmail.com>
2026-08-09 18:14:21 +0800

    Create Plugin.kt

---
+++ sakau/src/main/java/com/sakau2/twiceflix/Plugin.kt
@@ -0,0 +1,13 @@
+package com.sakau4.twiceflix
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class TwiceflixPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Registers your provider when the app loads the plugin
+        registerMainAPI(TwiceflixProvider())
+    }
+}
