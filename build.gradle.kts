3239c3177aab33fed1fdda50f0218c49b2bdadd1
boestar234-debug <boestar234@gmail.com>
2026-08-09 19:12:23 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+
 buildscript {
     repositories {
         google()
@@ -7,6 +9,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        // Required for the 'make' command to exist
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
     }
 }
@@ -17,4 +20,10 @@ allprojects {
         mavenCentral()
         maven("https://jitpack.io")
     }
+}
+
+apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+extensions.configure<CloudstreamExtension>("cloudstream") {
+    setRepo("boestar234-debug", "sakau4", "github")
 }
